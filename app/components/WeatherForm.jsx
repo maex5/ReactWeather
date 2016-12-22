ecea309ca3e7d1de20ed4e7924e3d66bbf385fb5
Max Salmi <max.salmi@me.com>
2016-12-22 16:03:24 +0200

    Add css-file & update classnames

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" placeholder="Enter city name" ref="location"/>
+          <input type="search" placeholder="Search weather by city" ref="location"/>
           <button className="expanded hollow button">Get Weather</button>
         </form>
       </div>
